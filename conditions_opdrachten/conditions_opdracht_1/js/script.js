afdbcb07aac1ffef3ea18dd4822bd388fa8315d1
RienPost <rien@rienpost.nl>
2019-12-10 14:44:37 +0100

    Opdracht 1 Loops

--- conditions_opdrachten/conditions_opdracht_1/js/script.js
+++ conditions_opdrachten/conditions_opdracht_1/js/script.js
@@ -1,1 +1,20 @@
 //javascript code
+
+/*var lijst = 0;
+
+for (var i = 0; i < 5; i++){
+    console.log(i);
+    //document.getElementById("output").innerHTML = i;
+   
+    if(i == 3){
+        console.log("drie");
+    }
+
+}*/
+
+var saldo = 24;
+
+if(saldo < 25){
+    console.log("Sorry, je hebt te weinig saldo!");
+}
+
