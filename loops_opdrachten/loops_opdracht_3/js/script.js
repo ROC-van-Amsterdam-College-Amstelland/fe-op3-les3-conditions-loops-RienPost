6bd78e99b7bc8a6917daba18161bce1cbdacc254
RienPost <rien@rienpost.nl>
2019-12-10 14:57:32 +0100

    Loops opdracht 3

--- loops_opdrachten/loops_opdracht_3/js/script.js
+++ loops_opdrachten/loops_opdracht_3/js/script.js
@@ -1,1 +1,12 @@
 //javascript code
+
+for (var i = 1;i < 51;i++){
+    console.log (i);
+    if (i == 20){
+        console.log ("Dit zijn mooie getallen!");
+    }
+
+    if (i == 40){
+        console.log ("Dit zijn mooie getallen!");
+    }
+}
